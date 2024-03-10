ea4891b888c8dbe125b9b6e65934f9c91d21aebb
Cristea Florinela <cristeaflorinela89@gmail.com>
2024-03-10 19:44:38 +0200

    Rezolvare Tema2

--- lab2/lab_dut/instr_register_pkg.sv
+++ lab2/lab_dut/instr_register_pkg.sv
@@ -19,11 +19,14 @@ package instr_register_pkg;
   typedef logic signed [31:0] operand_t;
   
   typedef logic [4:0] address_t;
+
+  typedef logic signed [63:0] result_t; 
   
   typedef struct {
     opcode_t  opc;
     operand_t op_a;
     operand_t op_b;
+    result_t result;
   } instruction_t;
 
 endpackage: instr_register_pkg
